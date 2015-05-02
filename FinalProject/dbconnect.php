b499c84ff4b12f870c8ccfb3cde2413e168e36de
Erik Giberti <erik@af-design.com>
2015-05-02 12:28:41 -0500

    Grrrr... tabs!

--- FinalProject/dbconnect.php
+++ FinalProject/dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 
-	// include the passwords and db configuration
-	require 'setup.php';
+    // include the passwords and db configuration
+    require 'setup.php';
 
     // connect to the db server
-	$mysql = mysqli_connect($db_server, $db_username, $db_password, $db_schema);
+    $mysql = mysqli_connect($db_server, $db_username, $db_password, $db_schema);
     if (!$mysql) {
         die("Unable to connect to database server");
     }
