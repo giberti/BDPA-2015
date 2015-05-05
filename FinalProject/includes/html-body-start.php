5b884a0dfcdee4cd32686497ea33c4a2bbdcaad4
Erik Giberti <erik@af-design.com>
2015-05-04 21:12:33 -0500

    Get ready for boostrap and mobile views

--- FinalProject/includes/html-body-start.php
+++ FinalProject/includes/html-body-start.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <?php
     include 'html-head.php';
 ?>
--- FinalProject/includes/html-head.php
+++ FinalProject/includes/html-head.php
@@ -3,4 +3,5 @@
     <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/bdpa-specific.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
