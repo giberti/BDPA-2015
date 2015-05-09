21eeabb3ac974cb79ff49203d120afd62b02165d
Erik Giberti <erik@af-design.com>
2015-05-08 21:35:17 -0500

    Adding add/edit functionality for routes

--- FinalProject/routes.php
+++ FinalProject/routes.php
@@ -34,10 +34,11 @@ if (loggedInUser() && isset($_GET['action'])) {
     }
 }
 
-$routes = getUpcomingRoutes($offset, 100);
-
 // If this user is logged in, show the add/edit route form
 if (loggedInUser()) {
+
+    $displayStyle = 'none';
+
     $routeFields = array(
         'RouteID' => '',
         'UserID' => '',
@@ -50,9 +51,16 @@ if (loggedInUser()) {
         'DateRidden' => '',
         'DateAdded' => ''
     );
-    echo '<div class="container">';
-    echo '<form action="' . getFilename() . '?action=save" method="post">';
+
     if ($editRoute) {
+        $displayStyle = 'block';
+    }
+
+    echo '<div class="container" id="addEditForm" style="display:' . $displayStyle. ';">';
+    echo '<form action="' . getFilename() . '?action=save" method="post">';
+
+    if ($editRoute) {
+        $displayStyle = 'block';
         echo '<h2>Editing this route</h2>';
     } else {
         echo '<h2>Add a new route!</h2>';
@@ -89,5 +97,60 @@ if (loggedInUser()) {
     echo '</div>';
 }
 
+$routes = getUpcomingRoutes($offset, $pageSize + 1);
+
+echo '<div class="container">';
+echo '<div class="row">';
+echo '<div class="col-md-12">';
+echo '<h1>Routes</h1>';
+if (loggedInUser() && !$editRoute) {
+    echo '<p>';
+    echo '<a id="addNewLink" onclick="showAddEditForm();">Add new route</a>';
+    echo '</p>';
+}
+
+foreach ($routes as $route) {
+    echo '<div class="row">';
+    if (loggedInUser()) {
+        ?>
+        <div class="col-xs-2">
+            <div class="btn-group-xs">
+                <a href="<?php echo getFilename(); ?>?action=edit&RouteID=<?php echo $route['RouteID']; ?>&offset=<?php echo $offset; ?>" class="btn btn-primary" aria-label="Left Align">
+                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                </a>
+                <a href="<?php echo getFilename(); ?>?action=delete&RouteID=<?php echo $route['RouteID']; ?>&offset=<?php echo $offset; ?>" class="btn btn-danger" aria-label="Left Align" onclick="return confirm('Are you sure you want to delete this route?');">
+                    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                </a>
+            </div>
+        </div>
+    <?php
+    }
+    echo '<div class="col-xs-10">';
+
+    echo '<h4>' . htmlentities($route['Name']) .  ' (' . htmlentities($route['Difficulty']) . ')</h4>';
+    echo '<p>' . htmlentities($route['Type']) . ' ' . round($route['Distance'], 1) . ' miles</p>';
+    if (strlen($route['Description']) < 100) {
+        echo '<p>' . htmlentities($route['Description']) . '</p>';
+    } else {
+        echo '<p>' . htmlentities(substr($route['Description'], 0, 95)) . '...</p>';
+    }
+    echo '</div>';
+    echo '</div>';
+}
+
+// Show pagination information
+echo '<p>';
+if ($offset > 0) {
+    echo '<a href="' . getFilename() . '?offset=' . ($offset - $pageSize) . '"> &lt; Back</a>';
+    echo ' ';
+}
+if (count($routes) > $pageSize) {
+    echo '<a href="' . getFilename() . '?offset=' . ($offset + $pageSize) . '">Next &gt;</a>';
+}
+echo '</p>';
+
+echo '</div>';
+echo '</div>';
+echo '</div>';
 
 include 'includes/html-body-end.php';
